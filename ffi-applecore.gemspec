e66eed087567bce2bc1415d8729e7f75c45b0b18
Kenneth Keiter <ken@kenkeiter.com>
2010-11-26 17:19:39 -0800

    Initial release.

---
+++ ffi-applecore.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+  s.name = %q{ffi-applecore}
+  s.version = "0.0.1"
+  s.date = %q{2010-11-26}
+  s.authors = ["Ken Keiter"]
+  s.email = %q{noise@kenkeiter.com}
+  s.summary = %q{Provides access to launchd and Apple System Logger.}
+  s.homepage = %q{http://kenkeiter.com/}
+  s.description = %q{Access to launchd and Apple System Logger.}
+  s.files = [ "README.rdoc", "Changelog", "LICENSE", "lib/ffi-applecore.rb", "lib/libapplecore.dylib"]
+end
---
+++ lib/ffi-applecore.rb
@@ -0,0 +1,83 @@
+require 'singleton'
+require 'ffi'
+module Apple
+  
+  module CoreServices
+    extend FFI::Library
+    ffi_lib 'applecore'
+
+    ASL_OPT_STDERR = 0x00000001
+    ASL_OPT_NO_DELAY = 0x00000002
+    ASL_OPT_NO_REMOTE = 0x00000004
+
+    attach_function :asl_begin, [:string, :string], :pointer
+    attach_function :asl_add_file, [:pointer, :int], :int
+    attach_function :asl_remove_file, [:pointer, :int], :int
+    attach_function :asl_msg, [:pointer, :string, :uchar], :void
+    attach_function :asl_end, [:pointer], :void
+
+    attach_function :launchd_register, [], :int # careful.. segfaults if not part of ld.
+  end
+  
+  class Launchd
+    include Singleton
+    
+    attr_reader :registered
+    
+    def initialize
+      @registered = false
+    end
+    
+    def register
+      unless @registered
+        if CoreServices::launchd_register() == 0
+          @registered = true
+        end
+      end
+    end
+    
+  end
+  
+  class SystemLogger
+    
+    def initialize(app_name, facility, dest=nil)
+      @asl = CoreServices::asl_begin(app_name, facility)
+      @log_fp = nil
+      
+      unless dest.nil?
+        if dest.is_a? String
+          @log_fp = File.new(dest,  "w+")
+          dest = @log_fp.to_i
+          at_exit{ @log_fp.close } # attach close handler
+        end
+        CoreServices::asl_add_file(@asl, dest.to_i)
+      end
+      
+      at_exit{ close }
+    end
+    
+    def log(sev, msg)
+      CoreServices::asl_msg(@asl, msg, sev)
+    end
+    
+    def close
+      @log_fp.close if @log_fp
+      CoreServices::asl_end(@asl)
+    end
+    
+    def emergency(msg) log(0, msg); end
+    def alert(msg) log(1, msg); end
+    def critical(msg) log(2, msg); end
+    def error(msg) log(3, msg); end
+    def warning(msg) log(4, msg); end
+    def notice(msg) log(5, msg); end
+    def info(msg) log(6, msg); end
+    def debug(msg) log(8, msg); end
+    
+    def <<(msg)
+      debug(msg)
+    end
+    
+  end
+  
+end
